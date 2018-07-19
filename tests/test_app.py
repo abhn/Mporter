41ae828c1edf5bf777f8125762acfef0d2b61469
Abhishek Nagekar <abhishek@nagekar.com>
2018-07-19 12:58:37 +0530

    added basic tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,12 @@
-from testing.postgresql import Postgresql
+from flask import Flask
 import pytest
 from app.factory import create_app
 from flask_sqlalchemy import SQLAlchemy
 from app.app_config import DB_URL_TEST
 from app.db import db_config, _db
-
+from app.celery_utils import make_celery
+from celery import Celery
+from utils import  get_env_var
 
 from app.models import Tasks, Mentees, Mentors
 
@@ -71,24 +73,106 @@ def test_admin_landing(testapp):
 
 def test_db_instance_of_sqlalchemy(db):
     """test if db is not None, and is an instance of SQLalchemy class"""
+
     assert isinstance(db, SQLAlchemy)
 
 
 def test_db_modal_create(session):
-    # task = Tasks(task='this is a test task', mentee_id=1)
+    """test db create"""
+
     mentor = Mentors(mentor_name='test123mentor', mentor_email='test1234@test1234.com')
     mentee = Mentees(mentee_name='test123mentee')
 
-    # session.add(task)
-    session.add(mentor)
-    session.add(mentee)
+    mentor.mentee.append(mentee)
+
+    session.add_all([mentor])
+    session.commit()
+
+    task = Tasks(task='this is a test task', mentee_id=mentee.id)
+
+    session.add(task)
+    session.commit()
+
+    assert mentor and mentee and task in session
+
+
+def test_db_modal_read(session):
+    """test db reads"""
+
+    mentor = Mentors(mentor_name='test123mentor', mentor_email='test1234@test1234.com')
+
+    mentee1 = Mentees(mentee_name='test123mentee')
+    mentee2 = Mentees(mentee_name='test456mentee')
+
+    mentor.mentee.append(mentee1)
+    mentor.mentee.append(mentee2)
+
+    session.add_all([mentor])
+    session.commit()
+
+    task1 = Tasks(task='task 1', mentee_id=mentee1.id)
+    task2 = Tasks(task='task 2', mentee_id=mentee2.id)
+
+    session.add_all([task1, task2])
 
     session.commit()
 
-    # assert task in session
-    assert mentor in session
-    assert mentee in session
+    mentors_var = Mentors.query.all()
+    # one row in mentors table
+    assert len(mentors_var) == 1
+
+    mentees_var = Mentees.query.all()
+    # two row in mentees table
+    assert (len(mentees_var)) == 2
+
+    mentor_to_check = mentors_var[0]
+    assert mentor_to_check.mentor_name == 'test123mentor' and mentor_to_check.id > 0
+
+    assert len(mentor_to_check.mentee) == 2
+
+    assert mentor_to_check.mentee[0].mentee_name == 'test123mentee' \
+        and mentor_to_check.mentee[1].mentee_name == 'test456mentee'
+
+    tasks_var = Tasks.query.all()
+    assert len(tasks_var) == 2
+
+    for task in tasks_var:
+        assert task.mentee_id > 0
 
 
-def test_db_modal_read(db):
-    pass
+def test_factory_create_app():
+    """test if app factory returns an instance of Flask"""
+
+    app_instance = create_app()
+
+    assert isinstance(app_instance, Flask)
+
+
+def test_factory_make_celery():
+    """test if celery factory returns an instance of Celery"""
+
+    app_instance = create_app()
+    celery_instance = make_celery(app_instance)
+
+    assert isinstance(celery_instance, Celery)
+
+
+def test_env_vars():
+    """test if the critical env variables are available in the environment"""
+
+    CELERY_BROKER_URL = get_env_var('CELERY_BROKER_URL')
+
+    DB_URL = get_env_var('SQLALCHEMY_DATABASE_URI')
+    DB_URL_TEST = get_env_var('SQLALCHEMY_DATABASE_URI_TEST')
+
+    SECRET_KEY = get_env_var('MPORTER_SECRET')
+
+    MAILGUN_KEY = get_env_var('MAILGUN_KEY')
+    MAILGUN_SANDBOX = get_env_var('MAILGUN_SANDBOX')
+
+    assert CELERY_BROKER_URL is not None
+    assert DB_URL is not None
+    assert DB_URL_TEST is not None
+    assert SECRET_KEY is not None
+    assert MAILGUN_KEY is not None
+    assert MAILGUN_SANDBOX is not None
